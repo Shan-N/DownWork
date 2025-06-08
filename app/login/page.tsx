4d4024261704211979c903da572364624c569ad7
Shan-N <shantanu.neve2000@gmail.com>
2025-06-08 11:00:04 +0530

    completed login frontend

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,49 +1,14 @@
 
+import { LoginForm } from "./components/login-form";
 
 
-const SignupPage = () => {
+const LoginPage = () => {
     return (
-        <div className="flex h-screen w-full items-center justify-center">
-        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
-            <form>
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="username">Username</label>
-                <input
-                type="text"
-                id="username"
-                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your username"
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="email">Email</label>
-                <input
-                type="email"
-                id="email"
-                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your email"
-                />
-            </div>
-            <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="password">Password</label>
-                <input
-                type="password"
-                id="password"
-                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter your password"
-                />
-            </div>
-            <button
-                type="submit"
-                className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition duration-200"
-            >
-                Sign Up
-            </button>
-            </form>
+        <div className="flex h-screen w-full">
+            <LoginForm className="dark md:max-w-1/2" />
+             {/* <span className="flex flex-row font-bold text-xl justify-center tracking-wider text-white"><MoveDown className="size-5"/>Work</span> */}
         </div>
-        </div>
-    );
+    )
 }
 
-export default SignupPage;
+export default LoginPage;
