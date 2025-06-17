1f36c5464c88da9e2bfd59807b6d082004622962
Shan-N <shantanu.neve2000@gmail.com>
2025-06-17 19:17:14 +0530

    hope route

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -a  # Automatically export all variables
+source .env.production
+set +a
+
+docker build \
+  --build-arg NEXT_PUBLIC_BASE_URL=$NEXT_PUBLIC_BASE_URL \
+  --build-arg NEXT_PUBLIC_SUPABASE_URL=$NEXT_PUBLIC_SUPABASE_URL \
+  --build-arg NEXT_PUBLIC_SUPABASE_ANON_KEY=$NEXT_PUBLIC_SUPABASE_ANON_KEY \
+  -t shan117/downwork:latest .
