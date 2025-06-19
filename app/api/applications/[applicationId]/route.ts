da72a4779caec82df36e201bd44c458359fe4b2f
Shan-N <shantanu.neve2000@gmail.com>
2025-06-19 15:08:56 +0530

    refractored : applicationId

--- app/api/applications/[applicationId]/route.ts
+++ app/api/applications/[applicationId]/route.ts
@@ -27,15 +27,65 @@ export async function GET(
         .select("full_name")
         .eq("id", application.freelancer_id)
         .single<{ full_name: string }>();
+    const { data: project, error: projectError } = await supabase
+        .from("projects")
+        .select("title")
+        .eq("id", application.project_id)
+        .single<{ title: string }>();
+    if (projectError) {
+        return NextResponse.json({ error: projectError.message }, { status: 400 });
+    }
     if (freelancerError) {
         return NextResponse.json({ error: freelancerError.message }, { status: 400 });
     }
     const { full_name } = freelancer;
+    const { title } = project;
     const newApplication = {
         ...application,
         freelancer_name: full_name || null,
+        project_title: title || null,
     };
     return NextResponse.json(newApplication, { status: 200 });
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+    }
+}
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: Promise<{ applicationId: string }> }
+){
+    const { applicationId } = await params;
+    try {
+        const supabase = await createClient();
+        const { data: { user }, error } = await supabase.auth.getUser();
+        if (error) {
+            return NextResponse.json({ error: error.message }, { status: 400 });
+        }
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+        const { data: userRole, error: userRoleError } = await supabase
+            .from('profiles')
+            .select('role')
+            .eq('id', user.id)
+            .single();
+        if (userRoleError) {
+            return NextResponse.json({ error: userRoleError.message }, { status: 400 });
+        }
+        if (userRole?.role !== 'client') {
+            return NextResponse.json({ error: "Unauthorized action" }, { status: 403 });
+        }
+        const { error: deleteError } = await supabase
+            .from('applications')
+            .delete()
+            .eq('id', applicationId);
+        if (deleteError) {
+            return NextResponse.json({ error: deleteError.message }, { status: 400 });
+        }
+        return NextResponse.json({ message: "Application deleted successfully" }, { status: 200 });
     } catch (error) {
         if (error instanceof Error) {
             return NextResponse.json({ error: error.message }, { status: 500 });
--- app/applications/[applicationId]/components/application-client.tsx
+++ app/applications/[applicationId]/components/application-client.tsx
@@ -1,6 +1,13 @@
 'use client';
+import { supabase } from "@/app/utils/supabase/client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Navbar from "@/components/ui/navbar";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import axios from "axios";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -8,8 +15,9 @@ import toast from "react-hot-toast";
 interface Application {
     id: string;
     project_id: string;
-    title: string;
+    project_title: string;
     freelancer_id: string;
+    freelancer_name: string;
     proposal: string;
     expected_budget: number;
     status: string;
@@ -24,8 +32,22 @@ interface ApplicationClientProps {
 const ApplicationClient = ({ applicationId }: ApplicationClientProps) => {
     const [application, setApplication] = useState<Application | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const [role, setRole] = useState<string | null>(null);
+    const router = useRouter();
 
     useEffect(() => {
+        const fetchRole = async () => {
+            try {
+                const { data } = await supabase.auth.getUser();
+                setRole(data?.user?.user_metadata?.role || null);
+            } catch (error) {
+                if (error instanceof Error) {
+                    toast.error("Error fetching user role: " + error.message);
+                } else {
+                    toast.error("An unexpected error occurred while fetching user role.");
+                }
+            }
+        }
         const fetchApplication = async () => {
             try {
                 setLoading(true);
@@ -44,8 +66,30 @@ const ApplicationClient = ({ applicationId }: ApplicationClientProps) => {
                 }
             }
         }
+        
+        fetchRole();
         fetchApplication();
     }, [applicationId]);
+
+    const handleDeleteApplication = async () => {
+        try {
+            const response = await axios.delete(`/api/applications/${applicationId}`);
+            if (response.status === 200) {
+                toast.success("Application deleted successfully.");
+                router.push("/applications");
+            }
+            else {
+                toast.error("Failed to delete application: " + response.statusText);
+            }
+
+        } catch (error) {
+            if (error instanceof Error) {
+                toast.error("Error deleting application: " + error.message);
+            } else {
+                toast.error("An unexpected error occurred while deleting application.");
+            }
+        }
+    }
 
     if (loading) {
         return <div>
@@ -62,16 +106,46 @@ const ApplicationClient = ({ applicationId }: ApplicationClientProps) => {
     return (
         <div className="dark text-white">
             <Navbar />
-            <div className="container mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">Application Details</h1>
-                <div className="shadow-md rounded-lg p-6">
-                    <h2 className="text-xl font-semibold mb-2">{application.title}</h2>
-                    <p><strong>Proposal:</strong> {application.proposal}</p>
-                    <p><strong>Expected Budget:</strong> ${application.expected_budget}</p>
-                    <p><strong>Status:</strong> {application.status}</p>
-                    <p><strong>Created At:</strong> {new Date(application.created_at).toLocaleDateString()}</p>
-                </div>
-            </div>
+            <Card className="max-w-2xl mx-auto my-8 p-6 shadow-lg">
+                <CardHeader className="text-2xl font-bold">
+                    <CardTitle>{application.project_title}</CardTitle>
+                    <span className="text-sm font-light text-gray-400">{application.status === "accepted" ? "Accepted" : "Pending"}</span>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <Badge variant='secondary' className="dark px-4">
+                           <Tooltip>
+                            <TooltipTrigger className="cursor-pointer">
+                                <span><Link href={`/profiles/${application.freelancer_id}`}>{application.freelancer_name}</Link></span>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <span>Visit Profile</span>
+                            </TooltipContent>
+                           </Tooltip>
+                        </Badge>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="space-y-2">
+                            <span>Applied on : {new Date(application.created_at).toLocaleDateString()}</span>
+                        </div>
+                    </div>
+                    <div className="space-y-2">
+                        <span>{application.proposal}</span>
+                    </div>
+                    {
+                        role === 'freelancer' ? (
+                        <div className="flex justify-center items-center space-x-4">
+                            <Button variant="destructive" onClick={handleDeleteApplication} className="m-0">Withdraw Application</Button>
+                        </div>
+                        ) : (
+                        <div className="flex justify-center items-center space-x-4">
+                            <Button variant="secondary" className="m-0">Accept</Button>
+                            <Button variant="destructive" className="m-0">Reject</Button>
+                        </div>
+                        )
+                    }
+                </CardContent>
+            </Card>
         </div>
     );
 
